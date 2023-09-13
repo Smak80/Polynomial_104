d3179a6f55cf9eda14ec5babd0fda4a830a3f1c1
Sergey Makletsov <smaklets@kpfu.ru>
2023-09-13 11:39:29 +0300

    Полином. Начало.

--- src/main/kotlin/ru/smak/polynomial104/Main.kt
+++ src/main/kotlin/ru/smak/polynomial104/Main.kt
@@ -1,2 +1,26 @@
 package ru.smak.polynomial104
 
+import ru.smak.polynomial104.math.Polynomial
+import kotlin.random.Random
+
+//var t: Polynomial? = null
+fun main() {
+    val p1 = Polynomial(mapOf(1 to 5.0, 2 to -3.0, 5 to 1.0))
+    println(p1)
+    val p2 = Polynomial(0.0, 0.0, 7.0, -4.0)
+    println(p2)
+
+//
+//    val rnd = Random.nextBoolean()
+//    if (rnd) t = Polynomial(3.0, 9.0, 5.0)
+//    println(t?.let {
+//        print("Количество коэффициентов: ")
+//        it.coeffs.size
+//    })
+//
+//    val p = Polynomial(3.5, 5.3).also { println(it) }
+//    val pp = Polynomial(2.3).apply {
+//        a = 3
+//        b = 7
+//    }
+}
--- src/main/kotlin/ru/smak/polynomial104/math/Polynomial.kt
+++ src/main/kotlin/ru/smak/polynomial104/math/Polynomial.kt
@@ -1,4 +1,15 @@
 package ru.smak.polynomial104.math
 
-class Polynomial {
+class Polynomial(coeffs: Map<Int, Double>) {
+    private val _coeffs: MutableMap<Int, Double> = coeffs.toMutableMap()
+    val coeffs = _coeffs.toMap()
+
+    /* ---
+    var a = 1
+    var b = 4 */
+
+    constructor(vararg coeffs: Double) : this (coeffs.mapIndexed { index, value -> index to value }.filter { it.second != 0.0 }.toMap())
+    override fun toString() =
+        _coeffs.toList().joinToString(separator = "; ") { "${it.first}: ${it.second}" }
+
 }
