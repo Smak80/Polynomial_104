599dfbad509f6f6b01a7ef7906abb75651519824
Sergey Makletsov <smaklets@kpfu.ru>
2023-09-20 11:42:36 +0300

    Полином от 20.09.2023

--- src/main/kotlin/ru/smak/polynomial104/Main.kt
+++ src/main/kotlin/ru/smak/polynomial104/Main.kt
@@ -1,9 +1,8 @@
 package ru.smak.polynomial104
 
 import ru.smak.polynomial104.math.Polynomial
-import kotlin.random.Random
+import ru.smak.polynomial104.math.eq
 
-//var t: Polynomial? = null
 fun main() {
     val p1 = Polynomial(mapOf(-1 to 5.0, 2 to -3.0, 5 to 1.0))
     println(p1)
@@ -16,6 +15,22 @@ fun main() {
     val p5 = (p1 * p2).also { println(it) }
     val p6 = (p1 - p2).also { println(it) }
 
+    val polynomial = Polynomial( 2.0,4.0,1.0)
+    val polynomial2 = polynomial/2.0
+    println("Результат деления: $polynomial2")
+
+    println(polynomial(2.0))
+    println(polynomial[2])
+
+
+    val a = 0.6 + 0.1
+    val b = 0.8 - 0.1
+    println(a.eq(b))
+    println("a = $a, b = $b")
+    // val divisor = 2
+
+    //val result = polynomial.map { it/divisor }
+    // println("Результат деления полинома $polynomial на число $divisor: $result")
 //
 //    val rnd = Random.nextBoolean()
 //    if (rnd) t = Polynomial(3.0, 9.0, 5.0)
--- src/main/kotlin/ru/smak/polynomial104/math/Polynomial.kt
+++ src/main/kotlin/ru/smak/polynomial104/math/Polynomial.kt
@@ -1,5 +1,8 @@
 package ru.smak.polynomial104.math
 
+import kotlin.math.abs
+import kotlin.math.pow
+
 class Polynomial(coeffs: Map<Int, Double>) {
     private val _coeffs: MutableMap<Int, Double>
 
@@ -8,35 +11,55 @@ class Polynomial(coeffs: Map<Int, Double>) {
     var a = 1
     var b = 4 */
     init {
-        _coeffs = coeffs.filter { (k, v) -> v != 0.0 && k >= 0}.toMutableMap()
-        if (_coeffs.isEmpty()) _coeffs[0]=0.0
+        _coeffs = coeffs.filter { (k, v) -> v != 0.0 && k >= 0 }.toMutableMap()
+        if (_coeffs.isEmpty()) _coeffs[0] = 0.0
     }
 
     val coeffs = _coeffs.toMap()
 
-    constructor(vararg coeffs: Double) : this (coeffs.mapIndexed { index, value -> index to value }.toMap())
+    constructor(vararg coeffs: Double) : this(coeffs.mapIndexed { index, value -> index to value }.toMap())
+
+    operator fun times(scalar: Double) = Polynomial(_coeffs.map { (k, v) -> k to scalar * v }.toMap())
 
 
-
-    operator fun times(scalar: Double) = Polynomial(_coeffs.map { (k, v) -> k to scalar*v }.toMap())
+    operator fun div(scalar: Double) =
+        Polynomial(_coeffs.map { (k, v) -> if (scalar == 0.0) throw ArithmeticException("Division by zero") else k to 1.0 / scalar * v }
+            .toMap())
 
     operator fun plus(other: Polynomial) = Polynomial(_coeffs.toMutableMap().also {
-        other._coeffs.forEach{(k2,v2) -> it[k2] = v2 + (it[k2]?:0.0)}
-    } )
+        other._coeffs.forEach { (k2, v2) -> it[k2] = v2 + (it[k2] ?: 0.0) }
+    })
 
     operator fun minus(other: Polynomial) = Polynomial(_coeffs.toMutableMap().also {
-        other._coeffs.forEach{(k2,v2) -> it[k2] = -v2 + (it[k2]?:0.0)}
-    } )
+        other._coeffs.forEach { (k2, v2) -> it[k2] = -v2 + (it[k2] ?: 0.0) }
+    })
 
     operator fun times(other: Polynomial) = Polynomial(mutableMapOf<Int, Double>().also {
-        _coeffs.forEach{ (k1, v1) ->
+        _coeffs.forEach { (k1, v1) ->
             other._coeffs.forEach { (k2, v2) ->
-                it[k1+k2] = v1*v2 + (it[k1+k2]?:0.0)
+                it[k1 + k2] = v1 * v2 + (it[k1 + k2] ?: 0.0)
             }
         }
     })
 
+    operator fun invoke(scalar: Double) = _coeffs.entries.sumOf { (k, v) -> v * scalar.pow(k) }
 
-    override fun toString() =
-        _coeffs.toList().joinToString(separator = "; ") { "${it.first}: ${it.second}" }
+    operator fun get(degree: Int) = _coeffs[degree] ?: 0.0
+
+    override fun toString() = toString("x")
+
+    fun toString(arg:String) = buildString {
+
+        _coeffs.toSortedMap(reverseOrder()).forEach { (k, v) ->
+            append(if (v >= 0.0) if (_coeffs.keys.max() == k) "" else "+" else "-")
+            if (abs(v) != 1.0 || k == 0 ) append(abs(v))
+            if(k > 0) {
+                append(arg)
+                if (k > 1) append("^$k")
+            }
+        } //sort by key
+
+    }
 }
+
+fun Double.eq(other: Double, eps: Double) = abs(this - other) < eps
